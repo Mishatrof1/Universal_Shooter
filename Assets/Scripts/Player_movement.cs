817989ac61d53632d5eaee43c7328c790c459c3d
DESKTOP-2SC8JN0\YHWH <dreymentov@mail.ru>
2022-03-31 22:31:59 +0300

    Testing By Dmitriy 31/03/2022

---
+++ Assets/Scripts/Player_movement.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_movement : MonoBehaviour
+{
+    public GameObject PlayerObject;
+    public Vector3 PlayerPos;
+    public float Speed;
+    public float MaxSpeed;
+    public float SpeedUp;
+
+    float InputVertical = Input.GetAxis("Vertical");
+    float InputHorizontal = Input.GetAxis("Horizontal");
+    float InputJump = Input.GetAxis("Jump");
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
