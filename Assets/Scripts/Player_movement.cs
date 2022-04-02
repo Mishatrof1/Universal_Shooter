e16d70b8d6e69286d32d9369670a737464ccc191
DESKTOP-2SC8JN0\YHWH <dreymentov@mail.ru>
2022-04-02 21:58:05 +0300

    Update by Dmitriy 02/04/2022

--- Assets/Scripts/Player_movement.cs
+++ Assets/Scripts/Player_movement.cs
@@ -5,35 +5,80 @@ using UnityEngine;
 public class Player_movement : MonoBehaviour
 {
     public GameObject PlayerObject;
-    public Vector3 PlayerPos;
-    public float PlayerPosX;
-    public float PlayerPosY;
-    public float PlayerPosZ;
     public float Speed;
     public float MaxSpeed;
     public float SpeedUp;
-    public float RatioBraking = 0.1f;
+    public float RatioBraking;
+    public float PowerJump;
+    public float PowerGravity;
 
-    float InputVertical = Input.GetAxis("Vertical");
-    float InputHorizontal = Input.GetAxis("Horizontal");
-    float InputJump = Input.GetAxis("Jump");
+    CharacterController PlayerControl;
 
+    //bool isGround = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        PlayerControl = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        PlayerPos = new Vector3(PlayerPosX, PlayerPosY, PlayerPosZ);
+        PlayerControl = GetComponent<CharacterController>();
 
-        //PlayerPosX;
+        float InVertical = Input.GetAxis("Vertical");
+        float InHorizontal = Input.GetAxis("Horizontal");
+        float InJump = Input.GetAxis("Jump");
+        float InSquat = Input.GetAxis("Squat");
 
-        //PlayerPosY;
+        float PlayerHeight = PlayerControl.height;
+        float PlayerHeightStand = PlayerHeight;
+        float PlayerHeightSquat = PlayerHeight * 0.6f;
 
-        //PlayerPosZ;
+        float JumpSpeed = 0f;
+
+        bool Jumped = false;
+
+        if(InSquat != 0)
+        {
+            PlayerHeight = PlayerHeightSquat;
+            PlayerObject.GetComponent<Transform>().localScale = new Vector3(1, PlayerHeightSquat, 1); // Для теста "приседаний"
+        }
+        else
+        {
+            PlayerHeight = PlayerHeightStand;
+            PlayerObject.GetComponent<Transform>().localScale = new Vector3(1, PlayerHeightStand, 1); // Для теста "приседаний"
+        }
+
+        if((PlayerControl.isGrounded == true) && (InJump != 0))
+        {
+            Jumped = true;
+            JumpSpeed = PowerJump;
+        }
+
+        if(Jumped == true)
+        {
+            if (JumpSpeed > 0)
+            {
+                JumpSpeed = JumpSpeed - (5 * Time.deltaTime);
+            }
+            else
+            {
+                Jumped = false;
+            }
+            
+        }
+
+        PlayerControl.Move(Vector3.up * JumpSpeed);
+
+        PlayerControl.Move(Vector3.forward * InVertical * Speed);
+
+        PlayerControl.Move(Vector3.right * InHorizontal * Speed/2);
+    }
+
+    void FixedUpdate()
+    {
+        PlayerControl.Move(Vector3.down);
     }
 }
